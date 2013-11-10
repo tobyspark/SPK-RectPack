05c9d197b9897d4200a1a8774b3b9ac1115670f1
Toby Harris <operator@tobyz.net>
2013-11-10 17:58:43 +0100

    Initial Commit

---
+++ SPK-RectPack/SPK_RectPackPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  SPK_RectPackPlugIn.h
+//  SPK-RectPack
+//
+//  Created by Toby Harris on 10/11/2013.
+//  Copyright (c) 2013 *spark live. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface SPK_RectPackPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
